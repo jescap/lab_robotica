92c7935977e42251c1e805d646f902fe9fda2f30
Jesus Capitan <jcapitan@us.es>
2024-09-22 23:18:21 +0200

    Docker run scripts modified

--- docker/run_container_mac.bash
+++ docker/run_container_mac.bash
@@ -1,9 +1,5 @@
-
-xhost +local:docker
 docker run -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+    --env="DISPLAY=host.docker.internal:0" \
     --name lab_rob_container \
     --net=host \
     --privileged \
